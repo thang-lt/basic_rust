7023838e697ca977a07534fccb1f6f468434a955
ThangLe <thangle@teqnological.com>
2022-06-16 17:46:38 +0900

    Done ex1

--- VBI1/src/main.rs
+++ VBI1/src/main.rs
@@ -1,7 +1,7 @@
 fn get_i_in_arr(val:  i32, start_i : i32, org_arr: & [i32]) -> i32 {
-
-    for i in 0..org_arr.len()  {
-        println!("value = {}", org_arr[i]);
+    // Get index Sub_array's char in Org_array
+    let index = start_i as usize;
+    for i in index..org_arr.len()  {
         if val == org_arr[i] {
             return i.try_into().unwrap()
         }
@@ -10,14 +10,13 @@ fn get_i_in_arr(val:  i32, start_i : i32, org_arr: & [i32]) -> i32 {
 }
 
 fn check_arr(org_arr: & [i32], sub_arr: & [i32]) -> bool {
+    // sub array
     let mut org_i = 0;
-    let mut sub_i_in_org = 0;
+    let mut sub_i_in_org:i32;
     let mut res = true;
 
     for x in sub_arr {
-        println!("sub: {}", x);
         sub_i_in_org = get_i_in_arr(*x, org_i, org_arr);
-        println!("sub_i_in_org: {}", sub_i_in_org);
         if sub_i_in_org>-1{
             org_i = sub_i_in_org + 1;
         }else{
@@ -30,32 +29,13 @@ fn check_arr(org_arr: & [i32], sub_arr: & [i32]) -> bool {
     return res;
 }
 
-fn test() {
-    // Rust 2021:
-    
-    let array= [1, 2,3,5,6,8, 10, 11];
-    
-    // This iterates by reference:
-    for item in array.iter().enumerate() {
-        let (i, x): (usize, &i32) = item;
-        println!("array[{i}] = {x}");
-    }
-    
-    // This iterates by value:
-    for item in array.into_iter().enumerate() {
-        let (i, x): (usize, i32) = item;
-        println!("array[{i}] = {x}");
-    }
-    
-}
-
 fn main() {
     let org_arr = [1, 2,3,5,6,8, 10, 11];
-    let sub_arr = [6,8, 11, 10];
+    let sub_arr = [6,8, 10, 1];
     if check_arr( & org_arr,  & sub_arr){
-        println!("Tap con!!");
+        println!("Right!!");
     }else{
-        println!("Khong phai!");
+        println!("Not right!");
     };
     
 }
