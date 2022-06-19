fa14eadf5a9331fbe3384ceb22db48a98a45475b
ThangLe <thangle@teqnological.com>
2022-06-19 10:56:40 +0900

    delete target

--- VBI1/target/rls/debug/deps/VBI1-44699d14b3a79a9b.d
+++
@@ -1,5 +0,0 @@
-/Users/thangle/Documents/study/rust_demo/basic_rust/VBI1/target/rls/debug/deps/VBI1-44699d14b3a79a9b.rmeta: src/main.rs
-
-/Users/thangle/Documents/study/rust_demo/basic_rust/VBI1/target/rls/debug/deps/VBI1-44699d14b3a79a9b.d: src/main.rs
-
-src/main.rs:
--- VBI1/target/rls/debug/deps/VBI1-d8409b14497e27e3.d
+++
@@ -1,5 +0,0 @@
-/Users/thangle/Documents/study/rust_demo/basic_rust/VBI1/target/rls/debug/deps/VBI1-d8409b14497e27e3.rmeta: src/main.rs
-
-/Users/thangle/Documents/study/rust_demo/basic_rust/VBI1/target/rls/debug/deps/VBI1-d8409b14497e27e3.d: src/main.rs
-
-src/main.rs:
