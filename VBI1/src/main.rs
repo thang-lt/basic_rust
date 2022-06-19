5d7052ae8ee925e3af556ed2dbe2ac50ea88a73b
ThangLe <thangle@teqnological.com>
2022-06-19 10:48:24 +0900

    done ex1, remove target

--- VBI1/src/main.rs
+++ VBI1/src/main.rs
@@ -28,7 +28,7 @@ fn check_arr(org_arr: & [i32], sub_arr: & [i32]) -> bool {
     return res;
 }
 
-fn ex1cd (){
+fn ex1_draft (){
     let org_arr = [1, 6,3,5,6,8, 6, 11];
     let test_arr = [6,8, 6, 11];
     if check_arr( & org_arr,  & test_arr){
@@ -36,6 +36,48 @@ fn ex1cd (){
     }else{
         println!("The test array is not a child of root array!!");
     };
+}
+
+fn ex1 (){
+    let org_arr = [1, 6,3,5,6,8, 6, 11, 34, 67];
+    let sub_arr = [8, 6, 11];
+    let len_org_arr = org_arr.len();
+    let len_sub_arr = sub_arr.len();
+    let mut result:bool = false;
+
+    if len_org_arr >= len_sub_arr{
+        'outer: for org_item in org_arr.iter().enumerate(){
+            let (o_i, o_x): (usize, &i32) = org_item;
+            if len_org_arr - o_i >= len_sub_arr{
+                if *o_x == sub_arr[0]{
+                    
+                    let mut right_num = 0;
+                    // Check another element
+                    '_inner: for sub_item in sub_arr.iter().enumerate(){
+                        let (s_i, s_x): (usize, &i32) = sub_item;
+                        let mut org_v = org_arr[o_i+s_i];
+                        if org_v == *s_x{
+                            // Count numbers right element
+                            right_num += 1;
+
+                            if (right_num) == len_sub_arr{
+                                result = true;
+                                break 'outer;
+                            }
+                        }else{
+                            right_num = 0;
+                        } 
+                    }
+                }
+            }
+        }
+    }
+
+    if result {
+        println!("The test array is a child of root array!!!");
+    }else{
+        println!("The test array is not a child of root array!!");
+    }
 }
 
 // -------Exercise 2-----------
@@ -74,11 +116,8 @@ fn compare_sub_string(s1: &str, s2: String) -> (String, i32) {
 }
 
 fn main() {
-    // ex1();
+    ex1();
     // ex2();
-    let org_string = "This is a regular paragraph with the default style of Normal. This is a regular paragraph with the default style of Normal. This is a regular paragraph with the default style of Normal. This is a regular paragraph with the default style of Normal. This is a regular paragraph with the default style of Normal.";
-    let input = "is";
-    compare_sub_string(input, org_string);
     
 }
 
--- VBI1/target/debug/VBI1.d
+++
@@ -1,1 +0,0 @@
-/Users/thangle/Documents/study/rust_demo/basic_rust/VBI1/target/debug/VBI1: /Users/thangle/Documents/study/rust_demo/basic_rust/VBI1/src/main.rs
