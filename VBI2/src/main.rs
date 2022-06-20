3266ea8fa45c0d47230d50d5faeb750660a3dc2d
ThangLe <thangle@teqnological.com>
2022-06-20 19:39:39 +0900

    fix own

--- VBI2/src/main.rs
+++ VBI2/src/main.rs
@@ -55,49 +55,51 @@
 
 //Exercise 3
 // Mục đích: giải quyết vấn đề ownership and borrowing ko dùng clone()
-fn main() {
-    let mut values = vec![10, 11, 12];
-    let v = & values;
+// fn main() {
+//     let mut values = vec![10, 11, 12];
+//     let v = & values;
 
-    let mut max = 0;
+//     let mut max = 0;
     
-    for n in v {
-        max = std::cmp::max(max, *n);
-    }
+//     for n in v {
+//         max = std::cmp::max(max, *n);
+//     }
 
-    println!("max is {}", max);
-    println!("Converting to percentages of maximum value...");
+//     println!("max is {}", max);
+//     println!("Converting to percentages of maximum value...");
 
-    let v = &mut values;
-    for n in v {
-        *n = 100 * (*n) / max;
-    }
-    println!("values: {:#?}", values);
-}
+//     let v = &mut values;
+//     for n in v {
+//         *n = 100 * (*n) / max;
+//     }
+//     println!("values: {:#?}", values);
+// }
 
 
 //Exercise 4
 // Mục đích : giải quyết vấn đề ownership và borrowing ko dùng clone()
 // Logic hiện tại đang sai (cho 1 vec -> đảo chiều vector đó)
-// fn main(){
-//     let  a = vec![1,2,3,4,5];
-//     let  i = 0;
-//     let c = 0;
-//     loop {
-//         let (a, c) = test(a);
-//         println!("{}",c);
-//         if i >=5 {break;}
-//     }
-// }
+fn main(){
+    let mut a = vec![1,2,3,4,5];
+    println!("{:?}", &a);
+    let i = 0;
+    let c = 0;
+    loop {
+        let (a, c) = test(&mut a);
+        println!("{}",c);
+        if i >=5 {break;}
+    }
+}
 
-// pub fn test(mut a: Vec<u8>) -> (Vec<u8>, i32) {
-//     let mut b:Vec<u8>  = Vec::new();
-//     let mut c:u8 = 0;
-//     loop {
-//         if a.len() == 0 { break; }
-//         let d = a.pop().unwrap();
-//         c = c+d;
-//         b.push(d);
-//     }
-//     (b, c as i32)
-// }
+pub fn test(a: &mut Vec<u8>) -> (Vec<u8>, i32) {
+    println!("{:?}", a);
+    let mut b:Vec<u8>  = Vec::new();
+    let mut c:u8 = 0;
+    loop {
+        if a.len() == 0 { break; }
+        let d = a.pop().unwrap();
+        c = c+d;
+        b.push(d);
+    }
+    (b, c as i32)
+}
