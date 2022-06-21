4e3c1574edc5a126c3ad34f83958b4dc21e0a1bd
ThangLe <thangle@teqnological.com>
2022-06-21 18:43:06 +0900

    Done ex3

--- VBI3/src/main.rs
+++ VBI3/src/main.rs
@@ -3,10 +3,9 @@
 // sách thông tin sinh viên bao gồm tên sinh viên và điểm của sinh viên đó.
 // với mục đích thống kê kiểm tra giáo dục của ngôi trường này
 
-
 /*-----------------------------*/
 // Gợi ý:
-// Định nghĩa bằng struct, mọi người nên sử dụng HashMap 
+// Định nghĩa bằng struct, mọi người nên sử dụng HashMap
 // Tại sao lại sử dụng HashMap và ko phải Vec
 //https://doc.rust-lang.org/std/collections/struct.HashMap.html
 // struct School {
@@ -14,7 +13,7 @@
 // }
 
 // trong trường hợp này thì String : tên của sinh viên đó
-// u32 là điểm số 
+// u32 là điểm số
 
 // Một số yêu cầu như sau:
 
@@ -37,12 +36,11 @@
 // ví dụ: hiện tại danh sách gồm có (Alice, 3), (Bob, 10), (Charlie,3)
 // liệt kê danh sách học sinh có cùng 3 điểm : [Alice, Charlie]
 
-// Yêu cầu trả về: danh sách sinh viên là alphabet theo tên 
+// Yêu cầu trả về: danh sách sinh viên là alphabet theo tên
 
-// Gợi ý: 
+// Gợi ý:
 // ví dụ : Ban đầu [(Alice, 10), (Bob,2), (Eve,4), (Long,2)] -> [(Bob, 2), (Long,2), (Eve,4), (Alice,10)]
 //định nghĩa hàm "find_student" có tham số là điểm số -> trả về danh sách các sinh viên có cùng điểm số mong muốn
-
 
 // Các bạn phải vuợt qua một số test case như sau :
 
@@ -64,9 +62,8 @@
 // với điểm số 4 thì ta có sinh viên nào: -> [Alice, Bob] not [Bob ,Alice]
 // vì cần tên theo alphabet
 
-
 /*-----------------------------*/
-// Nếu mọi người làm xong rùi thì có thể làm advance hơn bằng cách 
+// Nếu mọi người làm xong rùi thì có thể làm advance hơn bằng cách
 // sử dụng Generic type cho điểm số không nhất thiết phải U32 nữa mà có thể "A+", "B+" chẳng hạn (string)
 /*-----------------------------*/
 
@@ -75,36 +72,65 @@
 use std::collections::HashMap;
 
 fn main() {
-    let school1 = School{};
 
-    println!("Hello, world!");
+    let mut school1: School = School::new();
+    school1.add(5, "Alice".to_string());
+    school1.add(10, "WIN".to_string());
+    school1.add(4, "Ws".to_string());
+    school1.add(4, "Ks".to_string());
+    
+
+    // let students = &school1.get_students();
+    // println!("students: {:?}", students);
+
+    // let grades = school1.grades();
+    // println!("grades: {:?}", grades);
+
+    let grade = school1.grade(4);
+    println!("grade: {:?}", grade);
+
 }
 
-
-
 pub struct School {
-    students: HashMap<String, u32>
+    students: HashMap<String, u32>,
 }
 
 impl School {
     pub fn new() -> School {
-        students = HashMap::new();
+        School {
+            students: HashMap::new(),
+        }
     }
 
-    pub fn add(&mut self, grade: u32, student: &str) {
-        *self.grade = grade;
-        *self.student = student;
+    pub fn add(&mut self, grade: u32, student: String) {
+        self.students.insert(student.parse().unwrap(), grade);
+    }
+
+    pub fn get_students(self) -> HashMap<String, u32>{
+        return self.students;
+    }
+
+    pub fn frind_grades_by_name(&self, student: String) {
+        let a = self.students.get(&student.to_string());
     }
 
     pub fn grades(&self) -> Vec<u32> {
-        unimplemented!()
-
+        let mut _grades: Vec<u32> = self.students.values().cloned().collect();
+        _grades.sort();
+        _grades.dedup();
+        _grades
     }
 
-
     pub fn grade(&self, grade: u32) -> Vec<String> {
-        unimplemented!()
-
+        let mut list = Vec::<String>::new();
         
+        for (student, gr) in &self.students {
+            if *gr == grade {
+                list.push(student.to_string())
+            }
+        }
+
+        list.sort();
+        list
     }
 }
