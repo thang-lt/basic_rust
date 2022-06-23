015aa15b1bf686a65b8981a911a20ea735560ecf
ThangLe <thangle@teqnological.com>
2022-06-23 23:03:37 +0900

    Done excercise class4

--- VBI4/src/main.rs
+++ VBI4/src/main.rs
@@ -3,10 +3,6 @@
 
 //https://doc.rust-lang.org/std/iter/trait.Iterator.html
 
-// struct Fibonacci {
-//     a: u32,
-//     b: u32,
-// }
 // Như mọi người đã biết Dãy Fibonacci có quy luật như sau
 // Dãy Fibonacci là dãy số bắt đầu bằng 0 với 1. Mọi số tiếp theo
 // đều là tổng của 2 số trước đó
@@ -15,37 +11,45 @@
 
 // Trong trường hợp bài toán này 
 // Khởi tạo ban đầu sẽ là 
-// struct Fibonacci {
-//     a = 1,
-//     b = 0,
-// }
 
 // Một số kiến thức để làm dc bài này: Trait, Generic Type, Associated type,
 // Gợi ý có sườn như sau:
 
-#[derive(Debug)]
-struct Fibonacci {
-    a: u32,
-    b: u32,
-}
+// #[derive(Debug)]
+// struct Fibonacci {
+//     a: u32,
+//     b: u32,
+// }
 
-impl Iterator for Fibonacci {
-    type Item = TODO!;
+// impl Iterator for Fibonacci {
+//     type Item = u32;
 
-    fn next(&mut self) -> Option<u32> {
-        todo!()
-    }
-}
+//     // Trả về số fibonaci tiếp theo dựa trên kiểu dữ liệu struct Fibonacci
+//     fn next(&mut self) -> Option<Self::Item> {
+//         let sum = self.a + self.b;
+//         self.a = self.b;
+//         self.b = sum;
+//         Some(self.a)
+//     }
+// }
 
-fn fibonacci_numbers() -> Fibonacci {
-    Fibonacci { a: 1, b: 0 }
-}
+// Khởi tạo ban đầu cho Fibonaci: 0, 1
+// fn fibonacci_numbers() -> Fibonacci {
+//     Fibonacci { a: 1, b: 0 }
+// }
 
-fn main() {
-    for number in fibonacci_numbers() {
-        println!("{}", number);
-    }
-}
+
+//     Vì struct Fibonacci có implement trait Iterator của Rust nên 
+// có thể dùng câu lệnh for dc
+// Câu lệnh for bản chất sẽ chuyển qua trait Iterator nên instance của
+// struct Fibonacci có thể duyệt được, 
+// Mỗi lần duyệt sẽ tự động chạy function signature next() trên
+// Nên cần implement hàm next() cho struct Fiboncci.
+// fn main() {
+//     for number in fibonacci_numbers() {
+//         println!("{}", number);
+//     }
+// }
 
 
 // Kết quả :
@@ -71,20 +75,20 @@ fn main() {
 // Bài 2: Lifetime
 // Yêu cầu: Sửa lỗi Lifetime 
 
-// use std::fmt;
-// struct StrDisplayable(Vec<&str>);
+use std::fmt;
+struct StrDisplayable<'a>(Vec<&'a str>);
 
-// impl fmt::Display for StrDisplayable {
-//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-//         for v in &self.0 {
-//             write!(f, "\n{}", v)?;
-//         }
-//         Ok(())
-//     }
-// }
+impl <'a> fmt::Display for StrDisplayable<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for v in &self.0 {
+            write!(f, "\n{}", v)?;
+        }
+        Ok(())
+    }
+}
 
-// fn main() {
-//         let vec: Vec<&str> = vec!["a","bc","def"];
-//         let vec_Foo = StrDisplayable(vec);
-//         println!("{}",vec_Foo);
-// }
+fn main() {
+        let vec: Vec<&str> = vec!["a","bc","def"];
+        let vec_Foo = StrDisplayable(vec);
+        println!("{}",vec_Foo);
+}
